e258d44fdf019c1ccf6b624e6380a0abac4b5a9e
Amaury Gz <gzamaury@hotmail.com>
2023-07-06 22:28:25 +0000

    chore: Basic test config

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// Header Parser Microservice
 const headerParserPath = '/api/whoami';
 const gettingIp = (req, res, next) => {
   req.ipaddress = req.ip;
@@ -59,7 +49,13 @@ app.get(
   headerParserHandler
 );
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const port = process.env.PORT || 3000;
+let server = app;
+
+if (process.env.NODE_ENV !== "test") {
+  server = app.listen(port, function () {
+    console.log("Your app is listening on port " + server.address().port);
+  });
+}
+
+module.exports = server;
---
+++ tests/api.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("../index");
+
+describe("GET /api/hello", () => {
+  test("should return a JSON object with greeting", async () => {
+    const response = await request(app).get("/api/hello");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ greeting: "hello API" });
+  });
+});
