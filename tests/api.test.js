ffe55660b3e1ec258af9def7213559fa59c18ea8
Amaury Gz <gzamaury@hotmail.com>
2023-07-06 23:47:06 +0000

    chore: Add test to /api/whoami endpoint

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,3 +8,21 @@ describe("GET /api/hello", () => {
     expect(response.body).toEqual({ greeting: "hello API" });
   });
 });
+
+describe("GET /api/whoami", () => {
+  test("should return a JSON object with ipaddress, language and software", async () => {
+    const expectedObj = {
+      ipaddress: "::ffff:127.0.0.1",
+      language: "en-US,en;q=0.8",
+      software:
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+    };
+    const response = await request(app)
+      .get("/api/whoami")
+      .set("Accept-Language", expectedObj.language)
+      .set("User-Agent", expectedObj.software);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expectedObj);
+  });
+});
